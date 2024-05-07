e322568eadbe30896a46cfde07d4d4dbf0b1018b
MARCOS LEANDRO ANTUNES <158173682+marcosordnael@users.noreply.github.com>
2024-05-07 11:25:45 -0300

    adicionando iframe

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -15,13 +15,21 @@ menu.addEventListener('click' , ()=>{
 overLay.addEventListener('click' , ()=>{
     menu.classList.remove('abrir-menu')
 })
-// Abrir Modal
-function abrirModal() {
-    document.getElementById('modal').style.display = 'block';
+// Abrir Janela 3
+function abrirJanela() {
+    document.getElementById('janela3').style.display = 'block';
   }
   
-  function fecharModal() {
-    document.getElementById('modal').style.display = 'none';
+  function fecharJanela() {
+    document.getElementById('janela3').style.display = 'none';
+  }
+// Abrir Janela 2
+function abrirJanela2() {
+    document.getElementById('janela2').style.display = 'block';
+  }
+  
+  function fecharJanela2() {
+    document.getElementById('janela2').style.display = 'none';
   }
   
 
